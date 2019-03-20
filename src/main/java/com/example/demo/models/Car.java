ca671ae02f891eda48eb949f68e1ae94d49f07a1
zhukoff-av <mr.zhukoff@gmail.com>
2019-03-20 15:08:27 +0100

    added integration test

--- src/main/java/com/example/demo/models/Car.java
+++ src/main/java/com/example/demo/models/Car.java
@@ -16,7 +16,7 @@ public class Car {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
+  private String id;
 
   private String name;
   private String email;
@@ -29,11 +29,11 @@ public class Car {
   private Date purchase_date;
   private boolean contact;
 
-  public Long getId() {
+  public String getId() {
     return id;
   }
 
-  public void setId(Long id) {
+  public void setId(String id) {
     this.id = id;
   }
 
---
+++ src/test/java/com/example/demo/controllers/CarControllerIT.java
@@ -0,0 +1,45 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Car;
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+
+public class CarControllerIT {
+
+  RestTemplate restTemplate = new RestTemplate();
+
+
+  @Test
+  public void testGetListOfProviders() throws Exception {
+    ResponseEntity<List<Car>> responseEntity =
+            restTemplate.exchange("http://localhost:8080/api/v1/models", HttpMethod.GET, null,
+                    new ParameterizedTypeReference<List<Car>>() {
+                    });
+    List<Car> actualList = responseEntity.getBody();
+    //validate
+    assertThat(actualList.size(), is(2));
+    List<String> actualIds = actualList.stream()
+            .map(car -> car.getId())
+            .collect(collectingAndThen(toList(), ImmutableList::copyOf));
+
+    assertThat(actualIds, containsInAnyOrder("9", "10"));
+  }
+}
