6a9690deb25dc62f5e07f1602d938c05253c11ab
zhukoff-av <mr.zhukoff@gmail.com>
2019-03-20 15:13:43 +0100

    add lombok dependency

--- src/main/java/com/example/demo/models/Car.java
+++ src/main/java/com/example/demo/models/Car.java
@@ -2,6 +2,7 @@ package com.example.demo.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +13,7 @@ import java.util.Date;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Data
 public class Car {
 
   @Id
@@ -29,75 +31,4 @@ public class Car {
   private Date purchase_date;
   private boolean contact;
 
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public boolean getContact() {
-    return contact;
-  }
-
-  public void setContact(boolean contact) {
-    this.contact = contact;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPhone() {
-    return phone;
-  }
-
-  public void setPhone(String phone) {
-    this.phone = phone;
-  }
-
-  public String getModel() {
-    return model;
-  }
-
-  public void setModel(String model) {
-    this.model = model;
-  }
-
-  public String getSerial_number() {
-    return serial_number;
-  }
-
-  public void setSerial_number(String serial_number) {
-    this.serial_number = serial_number;
-  }
-
-  public BigDecimal getPurchase_price() {
-    return purchase_price;
-  }
-
-  public void setPurchase_price(BigDecimal purchase_price) {
-    this.purchase_price = purchase_price;
-  }
-
-  public Date getPurchase_date() {
-    return purchase_date;
-  }
-
-  public void setPurchase_date(Date purchase_date) {
-    this.purchase_date = purchase_date;
-  }
 }
